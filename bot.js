13c826cd0aca5a51a9d0797a1a255ada098e081c
yossifahmed177 <44752698+yossifahmed177@users.noreply.github.com>
2018-11-05 11:42:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("508395065698418700")
+let channel =     client.channels.get("508937899916656641")
 setInterval(function() {
 channel.send(`:rose: الله و اكبر 1`);
 channel.send(`:rose: الله و اكبر 2`);
